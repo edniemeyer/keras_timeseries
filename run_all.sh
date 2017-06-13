09ef757e8ffd388bad3b413cd60c198a171c8053
edniemeyer <edniemeyer@gmail.com>
2017-06-13 16:55:05 -0300

    adding new files for linux and py35

---
+++ run_all.sh
@@ -0,0 +1,7 @@
+python3 main_convolutional.py 0
+python3 main_convolutional.py 1
+python3 main_convolutional.py 2
+python3 main_convolutional.py 3
+python3 main_convolutional.py 4
+python3 main_convolutional.py 5
+python3 main_convolutional.py 6
