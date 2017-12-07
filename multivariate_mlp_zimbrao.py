1e086ad95f3d2479f7b2b7f4efe4dc7ccfb50b52
edniemeyer <edniemeyer@gmail.com>
2017-12-07 13:58:25 -0200

    Removing import not needed

--- multivariate_mlp_zimbrao.py
+++ multivariate_mlp_zimbrao.py
@@ -26,9 +26,7 @@ from keras.layers.advanced_activations import *
 from keras.optimizers import RMSprop, Adam, SGD, Nadam
 from keras.initializers import *
 
-import seaborn as sns
 start_time = time.time()
-sns.despine()
 
 batch_size = 128
 nb_epoch = 4200
