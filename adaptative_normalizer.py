8e9bc6a4e82296a385aa08dd2726ce90bcd02e86
edniemeyer <edniemeyer@gmail.com>
2018-01-30 18:18:40 -0200

    Created methods for adaptive normalization

--- adaptative_normalizer.py
+++ adaptative_normalizer.py
@@ -11,23 +11,32 @@ EMB_SIZE = 1
 
 #USD-BRL
 dataframe = pandas.read_csv('minidolar/wdo.csv', sep = '|',  engine='python', decimal='.',header=0)
-dataset = dataframe['fechamento'].tolist()
+dataset = dataframe['fechamento']
 media  = dataframe['media'].tolist()
+
+ewm_dolar = dataset.ewm(span=5, min_periods=5).mean()
+
+
+#removendo NaN
+dataset = dataset.iloc[4:]
+ewm_dolar = ewm_dolar.iloc[4:]
+
 
 #BTC-USD
 btc = pandas.read_csv('btc-usd.csv', sep = ',',  engine='python', decimal='.',header=0)
-dataset_btc = btc['close'].tolist()
+dataset_btc = btc['close']
+ewm_btc = dataset_btc.ewm(span=5, min_periods=5).mean()
+
+
+#removendo NaN
+dataset_btc = dataset_btc.iloc[4:]
+ewm_btc = ewm_btc.iloc[4:]
 
 #print(dataset_btc)
 
-X, Y = split_into_chunks(dataset, TRAIN_SIZE, TARGET_TIME, LAG_SIZE, binary=False, scale=True)
-X, Y = np.array(X), np.array(Y)
-X_train, X_test, Y_train, Y_test = create_Xt_Yt(X, Y, percentage=0.5)
+X, Y, shift = split_into_chunks_adaptive(dataset, ewm_dolar, TRAIN_SIZE, TARGET_TIME, LAG_SIZE, binary=False, scale=True)
+X, Y, shift = np.array(X), np.array(Y), np.array(shift)
+X_train, X_test, Y_train, Y_test, shift_train, shift_test = create_Xt_Yt_adaptive(X, Y, shift, percentage=0.5)
 
 dados = X_train, X_test, Y_train, Y_test
 
-Xp, Yp = split_into_chunks(dataset, TRAIN_SIZE, TARGET_TIME, LAG_SIZE, binary=False, scale=False)
-Xp, Yp = np.array(Xp), np.array(Yp)
-X_trainp, X_testp, Y_trainp, Y_testp = create_Xt_Yt(Xp, Yp, percentage=0.5)
-
-dadosp = X_trainp, X_testp, Y_trainp, Y_testp
--- processing.py
+++ processing.py
@@ -82,6 +82,44 @@ def split_into_chunks(data, train, predict, step, binary=True, scale=True):
 
     return X, Y
 
+def split_into_chunks_adaptive(data, ewm, train, predict, step, binary=True, scale=True):
+    X, Y, shift = [], [], []
+    for i in range(0, len(data), step):
+        try:
+            x_i = data[i:i+train]
+            y_i = data[i+train+predict]
+            
+            # Use it only for daily return time series
+            if binary:
+                if y_i > 0.:
+                    y_i = [1., 0.]
+                else:
+                    y_i = [0., 1.]
+
+                if scale: x_i = (np.array(x_i) - np.mean(x_i)) / np.std(x_i)
+                
+            else:
+                timeseries = np.array(data[i:i+train+predict])
+                shift_i = np.array(ewm[i:i+1])[0]
+                if scale:
+                    timeseries = timeseries/shift_i
+                    y_i = timeseries[-1]
+                    #y_i = (y_i - np.mean(timeseries[:-1])) / np.std(timeseries[:-1])
+                    #x_i = (np.array(timeseries[:-1]) - np.mean(timeseries[:-1])) / np.std(timeseries[:-1])
+                    x_i = timeseries[:-1]
+                else:
+                    x_i = timeseries[:-1]
+                    y_i = timeseries[-1]
+            
+        except:
+            break
+
+        X.append(x_i)
+        Y.append(y_i)
+        shift.append(shift_i)
+
+    return X, Y, shift
+
 
 def shuffle_in_unison(a, b):
     # courtsey http://stackoverflow.com/users/190280/josh-bleecher-snyder
@@ -105,6 +143,19 @@ def create_Xt_Yt(X, y, percentage=0.8):
     Y_test = y[int(len(y) * percentage):]
 
     return X_train, X_test, Y_train, Y_test
+
+def create_Xt_Yt_adaptive(X, y, shift, percentage=0.8):
+    X_train = X[0:int(len(X) * percentage)]
+    Y_train = y[0:int(len(y) * percentage)]
+    shift_train = y[0:int(len(shift) * percentage)]
+    
+    #X_train, Y_train = shuffle_in_unison(X_train, Y_train)
+
+    X_test = X[int(len(X) * percentage):]
+    Y_test = y[int(len(y) * percentage):]
+    shift_test = y[int(len(shift) * percentage):]
+
+    return X_train, X_test, Y_train, Y_test, shift_train, shift_test
 
 
 from statsmodels.tsa.stattools import adfuller
