Fixed Adaptive Normalization

mlp_new.py
@@ -42,7 +42,7 @@ ewm_dolar = ewm_dolar.iloc[4:]
 
 
 batch_size = 128
-nb_epoch = 4200
+nb_epoch = 20000
 patience = 500
 look_back = 7
 
@@ -50,6 +50,11 @@ TRAIN_SIZE = 30
 TARGET_TIME = 1
 LAG_SIZE = 1
 EMB_SIZE = 1
+
+X, Y = split_into_chunks(dataset, TRAIN_SIZE, TARGET_TIME, LAG_SIZE, binary=False,
+                                             scale=False)
+X, Y = np.array(X), np.array(Y)
+X_trainp, X_testp, Y_trainp, Y_testp = create_Xt_Yt(X, Y,  percentage=0.35)
 
 def evaluate_model(model, name, n_layers, ep, normalization):
     #X_train, X_test, Y_train, Y_test = dataset
@@ -64,10 +69,7 @@ def evaluate_model(model, name, n_layers, ep, normalization):
         X_train, X_test, Y_train, Y_test, scaler = nn_zs(dataset, TRAIN_SIZE, TARGET_TIME, LAG_SIZE)
     if (normalization == 'DS'):
         X_train, X_test, Y_train, Y_test, maximum = nn_ds(dataset, TRAIN_SIZE, TARGET_TIME, LAG_SIZE)
-    X, Y = split_into_chunks(dataset, TRAIN_SIZE, TARGET_TIME, LAG_SIZE, binary=False,
-                                             scale=False)
-    X, Y = np.array(X), np.array(Y)
-    X_trainp, X_testp, Y_trainp, Y_testp = create_Xt_Yt(X, Y,  percentage=0.35)
+
 
 
     csv_logger = CSVLogger('output/%d_layers/%s.csv' % (n_layers, name))
@@ -84,7 +86,9 @@ def evaluate_model(model, name, n_layers, ep, normalization):
 
     model.compile(loss='mean_squared_error', optimizer=optimizer)
     
-    history = model.fit(X_train, Y_train, batch_size=batch_size, epochs=ep, verbose=0, validation_split=0.1, callbacks=[csv_logger,es])
+    #history = model.fit(X_train, Y_train, batch_size=batch_size, epochs=ep, verbose=0, validation_split=0.1, callbacks=[csv_logger,es])
+    history = model.fit(X_train, Y_train, batch_size=batch_size, epochs=ep, verbose=0, validation_split=0.1,
+                        callbacks=[csv_logger])
 
     #trainScore = model.evaluate(X_train, Y_train, verbose=0)
     #print('Train Score: %f MSE (%f RMSE)' % (trainScore, math.sqrt(trainScore)))
@@ -108,15 +112,20 @@ def evaluate_model(model, name, n_layers, ep, normalization):
     if (normalization == 'DS'):
         X_trainp2, X_testp2, new_train_predicted, new_predicted = nn_ds_den(X_train, X_test, trainPredict, testPredict, maximum)
 
-    np.savetxt("output/previsto.csv", new_predicted[1:])
-    np.savetxt("output/real.csv", Y_testp[:-1])
-    np.savetxt("output/previsto_treino.csv", new_train_predicted[1:])
-    np.savetxt("output/real_treino.csv", Y_trainp[:-1])
+    # np.savetxt("output/previsto.csv", new_predicted)
+    # np.savetxt("output/real.csv", Y_testp)
+    # np.savetxt("output/previsto_treino.csv", new_train_predicted)
+    # np.savetxt("output/real_treino.csv", Y_trainp)
+
+    # np.savetxt("output/x_test-meu.csv", X_testp)
+    # np.savetxt("output/y_test-meu.csv", Y_testp)
+    # np.savetxt("output/x_treino-meu.csv", X_trainp)
+    # np.savetxt("output/y_treino-meu.csv", Y_trainp)
 
     # calculate root mean squared error
-    trainScore = math.sqrt(mean_squared_error(new_train_predicted[1:], Y_trainp[:-1]))
+    trainScore = math.sqrt(mean_squared_error(new_train_predicted, Y_trainp))
     #print('Train Score: %f RMSE' % (trainScore))
-    testScore = math.sqrt(mean_squared_error(new_predicted[1:], Y_testp[:-1]))
+    testScore = math.sqrt(mean_squared_error(new_predicted, Y_testp))
     #print('Test Score: %f RMSE' % (testScore))
     epochs = len(history.epoch)
 
@@ -137,7 +146,8 @@ def __main__(argv):
     nonlinearities = ['sigmoid', 'relu', 'tanh']
     #nonlinearities = ['relu']
 
-    normalizations = ['AN', 'SW', 'MM', 'ZS', 'DS']
+    #normalizations = ['AN', 'SW', 'MM', 'ZS', 'DS']
+    normalizations = ['AN', 'SW']
     with open("output/%d_layers/compare.csv" % n_layers, "a") as fp:
         fp.write("-MINIDOLAR/MLP NN\n")
 
@@ -149,10 +159,10 @@ def __main__(argv):
     f_aux = 0
 
     #for name in nonlinearities:
-    #for normalization in normalizations:
-    for f in range(1,2):
-        name='relu'
-        normalization = 'AN'
+    for normalization in normalizations:
+    # for f in range(1,2):
+        name='tanh'
+        # normalization = 'AN'
         model = Sequential()
 
         model.add(Dense(12, input_shape = (TRAIN_SIZE, ), kernel_regularizer=regularizers.l2(0.01)))

mlp_zimbrao.py
@@ -42,10 +42,35 @@ test_shift = test['shift']
 test_target = test['f0']
 test_close = test[['v3','v7','v11','v15','v19','v23','v27','v31','v35','v39','v43','v47','v51','v55','v59','v63','v67','v71','v75','v79','v83','v87','v91','v95','v99','v103','v107','v111','v115','v119']]
 
+X_train, X_test, Y_train, Y_test = np.array(train_close), np.array(test_close), np.array(
+    train_target.values.reshape(train_target.size, 1)), np.array(test_target.values.reshape(test_target.size, 1))
+X_trainp, X_testp, Y_trainp, Y_testp = X_train + train_shift.values.reshape(train_shift.size,
+                                                                            1), X_test + test_shift.values.reshape(
+    test_shift.size, 1), Y_train + train_shift.values.reshape(train_shift.size, 1), Y_test + test_shift.values.reshape(
+    test_shift.size, 1)
+
+# np.savetxt("output/x_test.csv", X_testp)
+# np.savetxt("output/y_test.csv", Y_testp)
+# np.savetxt("output/x_treino.csv", X_trainp)
+# np.savetxt("output/y_treino.csv", Y_trainp)
+
+# X_train_n, X_test_n, Y_train_n, Y_test_n, scaler_train, scaler_test = [], [], [], [], [], []
+# for i in range(train_close.shape[0]):
+#     X_normalizado, scaler = minMaxNormalize(np.array(train_close)[i].reshape(-1, 1))  # shape(30,1)
+#     X_train_n.append(X_normalizado.reshape(-1))  # shape(30)
+#     Y_train_n.append(minMaxNormalizeOver(np.array(train_target)[i], scaler).reshape(-1))
+#     scaler_train.append(scaler)
+#
+# for i in range(test_close.shape[0]):
+#     X_normalizado, scaler = minMaxNormalize(np.array(test_close)[i].reshape(-1, 1))  # shape(30,1)
+#     X_test_n.append(X_normalizado.reshape(-1))  # shape(30)
+#     Y_test_n.append(minMaxNormalizeOver(np.array(test_target)[i], scaler).reshape(-1))
+#     scaler_test.append(scaler)
+
 
 batch_size = 128
-nb_epoch = 4200
-patience = 500
+nb_epoch = 20000
+#patience = 5000
 look_back = 7
 
 TRAIN_SIZE = 30
@@ -57,12 +82,10 @@ def evaluate_model(model, name, n_layers, ep):
     #X_train, X_test, Y_train, Y_test = dataset
     #X_trainp, X_testp, Y_trainp, Y_testp = dadosp
 
-    X_train, X_test, Y_train, Y_test =  np.array(train_close),  np.array(test_close),  np.array(train_target.values.reshape(train_target.size,1)),  np.array(test_target.values.reshape(test_target.size,1))
-    X_trainp, X_testp, Y_trainp, Y_testp = X_train+train_shift.values.reshape(train_shift.size,1), X_test+test_shift.values.reshape(test_shift.size,1), Y_train+train_shift.values.reshape(train_shift.size,1), Y_test + test_shift.values.reshape(test_shift.size,1)
-
+    # X_train, X_test, Y_train, Y_test = np.array(X_train_n), np.array(X_test_n), np.array(Y_train_n), np.array(Y_test_n)
 
     csv_logger = CSVLogger('output/%d_layers/%s.csv' % (n_layers, name))
-    es = EarlyStopping(monitor='loss', patience=patience)
+    #es = EarlyStopping(monitor='loss', patience=patience)
     #mcp = ModelCheckpoint('output/mnist_adaptative_%dx800/%s.checkpoint' % (n_layers, name), save_weights_only=True)
     #tb = TensorBoard(log_dir='output/mnist_adaptative_%dx800' % n_layers, histogram_freq=1, write_graph=False, write_images=False)
 
@@ -75,7 +98,7 @@ def evaluate_model(model, name, n_layers, ep):
 
     model.compile(loss='mean_squared_error', optimizer=optimizer)
     
-    history = model.fit(X_train, Y_train, batch_size=batch_size, epochs=ep, verbose=0, validation_split=0.1, callbacks=[csv_logger,es])
+    history = model.fit(X_train, Y_train, batch_size=batch_size, epochs=ep, verbose=0, validation_split=0.1, callbacks=[csv_logger])
 
     #trainScore = model.evaluate(X_train, Y_train, verbose=0)
     #print('Train Score: %f MSE (%f RMSE)' % (trainScore, math.sqrt(trainScore)))
@@ -85,10 +108,27 @@ def evaluate_model(model, name, n_layers, ep):
     # make predictions (scaled)
     trainPredict = model.predict(X_train)
     testPredict = model.predict(X_test)
-    
-    
+
+    # X_train_d, X_test_d, Y_train_d, Y_test_d = [], [], [], []
+    # for i in range(X_train.shape[0]):
+    #     X_denormalizado = minMaxDenormalize(X_train[i].reshape(-1, 1), scaler_train[i]).reshape(-1)
+    #     X_denormalizado = X_denormalizado + train_shift[i]
+    #     X_train_d.append(X_denormalizado)
+    #     Y_denormalizado = minMaxDenormalize(trainPredict[i].reshape(-1, 1), scaler_train[i]).reshape(-1)
+    #     Y_denormalizado = Y_denormalizado + train_shift[i]
+    #     Y_train_d.append(Y_denormalizado)
+    #
+    # for i in range(X_test.shape[0]):
+    #     X_denormalizado = minMaxDenormalize(X_test[i].reshape(-1, 1), scaler_test[i]).reshape(-1)
+    #     X_denormalizado = X_denormalizado + test_shift[i]
+    #     X_test_d.append(X_denormalizado)
+    #     Y_denormalizado = minMaxDenormalize(testPredict[i].reshape(-1, 1), scaler_test[i]).reshape(-1)
+    #     Y_denormalizado = Y_denormalizado + test_shift[i]
+    #     Y_test_d.append(Y_denormalizado)
+
     # invert predictions (back to original)
-    
+
+    # new_train_predicted, new_predicted = np.array(Y_train_d), np.array(Y_test_d)
     new_predicted = testPredict+test_shift.values.reshape(test_shift.size,1)
     new_train_predicted= trainPredict+train_shift.values.reshape(train_shift.size,1)
 

processing.py
@@ -92,7 +92,10 @@ def split_into_chunks_adaptive(data, ewm, train, predict, step, binary=True, sca
                 
             else:
                 timeseries = np.array(data[i:i+train+predict])
-                shift_i = np.array(ewm[i:i+1])[0]
+
+                if((i%train) == 0):
+                    j=i
+                shift_i = np.array(ewm[j:j+1])[0] # 30(window length) samples will be divided by same 'shift'
                 if scale:
                     timeseries = timeseries/shift_i
                     y_i = timeseries[-1]
@@ -158,29 +161,29 @@ def create_Xt_Yt_adaptive(X, y, shift, percentage=0.8):
     return X_train, X_test, Y_train, Y_test, shift_train, shift_test
 
 
-#from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import adfuller
 #check if timeseries is stationary
-# def test_stationarity(timeseries):
-#
-#     #Determing rolling statistics
-#     rolmean = pd.rolling_mean(timeseries, window=12)
-#     rolstd = pd.rolling_std(timeseries, window=12)
-#
-#     #Plot rolling statistics:
-#     orig = plt.plot(timeseries, color='blue',label='Original')
-#     mean = plt.plot(rolmean, color='red', label='Rolling Mean')
-#     std = plt.plot(rolstd, color='black', label = 'Rolling Std')
-#     plt.legend(loc='best')
-#     plt.title('Rolling Mean & Standard Deviation')
-#     plt.show(block=False)
-#
-#     #Perform Dickey-Fuller test:
-#     print('Results of Dickey-Fuller Test:')
-#     dftest = adfuller(timeseries.unstack(), autolag='AIC')
-#     dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
-#     for key,value in dftest[4].items():
-#         dfoutput['Critical Value (%s)'%key] = value
-#     print(dfoutput)
+def test_stationarity(timeseries):
+
+    #Determing rolling statistics
+    rolmean = timeseries.rolling(window=12,center=False).mean()
+    rolstd = timeseries.rolling(window=12,center=False).std()
+
+    #Plot rolling statistics:
+    orig = plt.plot(timeseries, color='blue',label='Original')
+    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
+    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show(block=False)
+
+    #Perform Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries.values, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print(dfoutput)
 
 
 