e78c68fc13c6d3290c43407713e207de0a44c783
eduardo <edniemeyer@gmail.com>
2018-02-14 23:39:17 -0200

    Added R-code for outliers removal

--- processing.py
+++ processing.py
@@ -366,3 +366,22 @@ def nn_an_den(X_train, X_test, Y_train, Y_test, scaler, shift_train, shift_test)
 
     X_train, X_test, Y_train, Y_test = np.array(X_train_d), np.array(X_test_d), np.array(Y_train_d), np.array(Y_test_d)
     return X_train, X_test, Y_train, Y_test
+
+
+#TODO: adapt to python
+# ts.outliers.boxplot <- function(data, alpha = 1.5)
+# {
+#   org = nrow(data)
+#   q = as.data.frame(lapply(data, quantile))
+#   n = ncol(data)
+#   for (i in 1:n)
+#   {
+#     IQR = q[4,i] - q[2,i]
+#     lq1 = q[2,i] - alpha*IQR
+#     hq3 = q[4,i] + alpha*IQR
+#     cond = data[,i] >= lq1 & data[,i] <= hq3
+#     data = data[cond,]
+#   }
+#   final = nrow(data)
+#   return (data)
+# }
