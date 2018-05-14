Fixing Decimal Scaling

normalizer.py
@@ -4,16 +4,16 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
 def decimalNormalize(x):
-  return (np.array(x) / 10**np.log10(max(x)))
+  return (np.array(x) / 10**np.ceil(np.log10(max(x))))
 
 def decimalNormalizeOver(x,maximum):
-    X = (np.array(x) / 10 ** np.log10(maximum))
+    X = (np.array(x) / 10 ** np.ceil(np.log10(maximum)))
     X[X > 1.0] = 1.0  # limit top
     return X
 
 
 def decimalDenormalize (x,maximum):
-  return (np.array(x)*(10**np.log10(maximum)))
+  return (np.array(x)*(10**np.ceil(np.log10(maximum))))
 
 def minMaxNormalize(x):
     scaler = MinMaxScaler(feature_range=(-1, 1))