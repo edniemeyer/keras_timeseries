96a0da582f95bba80c9549151d02b139ca722390
edniemeyer <edniemeyer@gmail.com>
2018-08-07 16:57:54 -0300

    changing to 1000 epochs

--- mlp_rain.py
+++ mlp_rain.py
@@ -42,7 +42,7 @@ dataframe = pandas.read_csv('annual-rainfall-at-fortaleza-bra.csv', sep=',', eng
 dataset_original = dataframe['rainfall']
 
 batch_size = 64
-nb_epoch = 100
+nb_epoch = 1000
 patience = 1000
 TARGET_TIME = 1
 LAG_SIZE = 1
